570a6bb35360a55fcf6a5ac9e405c7fa9718267e
Fabrizio <fabri.galli@outlook.com>
2022-03-22 14:57:00 -0300

    Agregado template de bootstrap

--- bioecomm/urls.py
+++ bioecomm/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import  settings
 
 urlpatterns = [
     path('shop/', include('shop.urls')),
+    path('ecomadmin/', include('ecomadmin.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
