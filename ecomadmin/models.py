e2409c00c16cd74b22e61664999b65590e2c03bc
Fabrizio <fabri.galli@outlook.com>
2022-03-24 10:40:14 -0300

    Agregado modelos de productos

--- ecomadmin/models.py
+++ ecomadmin/models.py
@@ -1,3 +1,78 @@
 from django.db import models
 
-# Create your models here.
+
+class GrupoProducto(models.Model):
+	id_grupo = models.CharField(max_length=2, validators=[MinLengthValidator(2)], primary_key=True)
+	descripcion = models.CharField(max_length=200)
+
+	def __str__(self):
+		return f'{self.id_grupo} - {self.descripcion}'
+
+
+class LineaProducto(models.Model):
+	id = models.CharField(max_length=1, validators=[MinLengthValidator(1)], primary_key=True)
+	descripcion = models.CharField(max_length=200)
+
+	def __str__(self):
+		return f'{self.id} - {self.descripcion}'
+
+
+class ClasificacionProducto(models.Model):
+	id = models.CharField(max_length=2, validators=[MinLengthValidator(2)], primary_key=True)
+	descripcion = models.CharField(max_length=200)
+
+	def __str__(self):
+		return f'{self.id} - {self.descripcion}'
+
+
+class Producto(models.Model):
+	"""
+	To do:
+	Ver tabla para vincular formulas con presentaciones
+
+	Crear campos:
+	nombre
+	sumario
+	especies
+	categoria
+	subcategoria (puede ser varios, igual que especies)
+
+	detalle (text field)
+
+
+	"""
+
+
+	# Campos migrados de VFP
+	linea = models.ForeignKey(LineaProducto, on_delete=models.PROTECT, null=True) # SET NOT NULL
+	clasificacion = models.ForeignKey(ClasificacionProducto, on_delete=models.SET_NULL, null=True)
+
+
+	
+
+
+class PresentacionProducto(models.Model):
+	producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+	# presentacion_descripcion 
+
+	# Campos migrados de VFP
+	id_grupo = models.ForeignKey(GrupoProducto, on_delete=models.PROTECT)
+	id_producto = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
+	nombre_sis = models.CharField(max_length=200)
+	existencia_eu = models.IntegerField(default=0)
+	existencia_av = models.IntegerField(default=0)
+	existencia_mr = models.IntegerField(default=0)
+	existencia_ml = models.IntegerField(default=0)
+	precio = models.IntegerField(default=0)
+	en_oferta = models.BooleanField(default=False)
+	precio_oferta = models.IntegerField(default=0)
+	tasa = models.IntegerField(choices=[(0,0),(5,5),(10,10)])
+
+
+	class Meta:
+		unique_together = (('grupo', 'id_producto'),)
+
+	def __str__(self):
+		return self.descripcion
+
+
